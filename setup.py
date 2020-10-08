fb2a80f458e1f078f1dd976fabcff49da7edb6ae
Greg Albrecht <github.com@undef.net>
2020-10-08 15:55:28 -0700

    changed setup requires to tests requires to remove install dep on coverage, and thus, wheel

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import sys
 import setuptools
 
 __title__ = 'spotcot'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __author__ = 'Greg Albrecht W2GMD <oss@undef.net>'
 __copyright__ = 'Copyright 2020 Orion Labs, Inc.'
 __license__ = 'Apache License, Version 2.0'
@@ -44,7 +44,7 @@ setuptools.setup(
     url='https://github.com/ampledata/spotcot',
     zip_safe=False,
     include_package_data=True,
-    setup_requires=[
+    tests_requires=[
         'coverage >= 3.7.1',
         'pytest'
     ],
