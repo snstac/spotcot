2b496c2f08e11eceb1c8d1944eecc5a6ca59c1ea
Jorge <jmarcorb@gmail.com>
2020-12-23 22:57:58 +0100

    adding support for spot feed passwords

--- spotcot/functions.py
+++ spotcot/functions.py
@@ -62,7 +62,10 @@ def spot_to_cot(message: spot_sdk.Message) -> pycot.Event:
 
 def create_spot_feed(api_key: str) -> spot_sdk.Feed:
     """Creates a Spot Message Feed."""
-    return spot_sdk.Feed(api_key)
+    if not isinstance(password, str)
+        return spot_sdk.Feed(api_key)
+    else:
+        return spot_sdk.Feed(api_key, password)
 
 
 def get_first_message(spot_feed: spot_sdk.Feed) -> spot_sdk.Message:
