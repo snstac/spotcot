045ee36c4ea58c625751605aa05b156b18a22ec5
Srevin Saju <srevinsaju@sugarlabs.org>
2021-03-11 13:23:03 +0300

    ci: include update information into the appimage

--- scripts/ci-ubuntu1604-build_appimage.sh
+++ scripts/ci-ubuntu1604-build_appimage.sh
@@ -37,4 +37,10 @@ python3 -m pip install -r requirements.txt
 python3 -m pip install .
 python3 -m pyappimage.cli build
 
- 
+export APPIMAGE_EXTRACT_AND_RUN=1
+wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod +x appimagetool-x86_64.AppImage
+VERSION="$(git describe --tags --always --match '*[0-9]*')" ./appimagetool*.AppImage \
+    squashfs-root -n \
+    -u 'gh-releases-zsync|ampledata|spotcot|continuous|spotcot-*.AppImage.zsync' \
+    spotcot-$(uname -m).AppImage
